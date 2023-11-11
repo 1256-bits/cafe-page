cbaed1f76a037c3b4be4f98e906841c678781d73
128bit <128bit@eclipso.eu>
2023-11-11 16:49:02 +0300

    Split webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -20,8 +20,8 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        test: /\.scss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
