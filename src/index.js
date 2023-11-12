c8a0b2c6199a49476bfa35e72669b59d9d03c59d
128bit <128bit@eclipso.eu>
2023-11-12 16:57:51 +0300

    Add const declarations in createHomePage()

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
-import "normalize.css" 
+import "normalize.css"
 import "./style.scss"// import after normalize.css to prevent overwriting
+//If I ever wonder why did they invent frameworks
+function createHomePage() {
+    const tab = document.createElement("div"); //return value
+    const h2 = document.createElement("h2"); //main header
+
+    //lorem ipsum section
+    const aboutSec = document.createElement("section");
+    const aboutH3 = document.createElement("h3");
+    const aboutP1 = document.createElement("p");
+    const aboutP2 = document.createElement("p");
+
+    //schedule section
+    const scheduleSec = document.createElement("section");
+    const scheduleH3 = document.createElement("h3");
+    const ul = document.createElement("ul");
+    const li = document.createElement("li");
+
+    //address section
+    const addrSec = document.createElement("section");
+    const addrH3 = document.createElement("h3");
+    const addrP = document.createElement("h3");
+    
+
+}
