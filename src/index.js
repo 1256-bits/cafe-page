9341bd607acbc327a47ebce7b255ce7dee86c1fc
128bit <128bit@eclipso.eu>
2023-11-14 15:41:01 +0300

    Attach about componenet generation to an event listener

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import "normalize.css"
 import "./style.scss"// import after normalize.css to prevent overwriting
-import createHomePage from "./components/about.js"
+import createAboutTab from "./components/about.js"
 
 const main = document.querySelector("main");
-main.appendChild(createHomePage());
+const about = document.getElementById("about-us");
+const testimonials = document.getElementById("testimonials");
+const thanks = document.getElementById("thanks");
+
+about.addEventListener("click", () => {
+  main.appendChild(createAboutTab());
+});
