1d313cda843f6f16a46c3a734cd1d35d580972e8
128bit <128bit@eclipso.eu>
2023-11-15 16:47:19 +0300

    Add createTestimonials, genReview

--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -1,4 +1,45 @@
 export default function createTestimonials() {
+  const reviews = [
+    {
+      pic: "./cat-pic-1.png",
+      message:
+        "This is a nice caffee for humans.I will recommend it to my friends.",
+    },
+    {
+      pic: "./cat-pic-2.png",
+      message: "This cafe is great! I like the foodm especially the fishes.",
+    },
+    {
+      pic: "./cat-pic-3.png",
+      message: "Meow",
+    },
+  ];
   const tab = document.createElement("div");
+  const h2 = document.createElement("h2");
+  const ul = document.createElement("ul");
+  tab.classList.add("tab", "testimonials");
+  h2.classList.add("center");
+  ul.classList.add("grid");
+  h2.innerText = "Client reviews:";
+  reviews.forEach((r) => ul.appendChild(genReview(r)));
+  tab.append(h2, ul);
   return tab;
+}
+
+function genReview(review) {
+  const li = document.createElement("li");
+  const img = document.createElement("img");
+  const quote = document.createElement("blockquote");
+  const p = document.createElement("p");
+
+  li.classList.add("grid", "center", "bg-primary");
+  quote.classList.add("bg-secondary");
+
+  img.src = review.pic;
+  img.alt = "client photo";
+  p.innerText = review.message;
+
+  quote.appendChild(p);
+  li.append(img, quote);
+  return li;
 }
